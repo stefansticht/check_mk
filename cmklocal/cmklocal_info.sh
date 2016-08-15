53dfe2eab5335466144133a5f1c08e8fb2eee04b
Stefan Sticht <sticht@black.y42.net>
2016-08-15 08:24:46 +0200

    first test

---
+++ cmklocal/cmklocal_info.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ver=""
+host=`hostname`
+kernel=`uname -r`
+if [ -e /etc/redhat-release ]; then ver=`head -1 /etc/redhat-release`
+elif type lsb_release > /dev/null 2>&1 ; then ver=`lsb_release -d`; ver=${ver#*:}; ver=${ver#"${ver%%[![:space:]]*}"}
+fi
+
+echo "0 Info - $host $ver $kernel"
+
