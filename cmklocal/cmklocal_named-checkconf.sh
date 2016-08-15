c06fcadbf5c112ee2422c19dac3e8e015cb173be
Stefan Sticht <sticht@black.y42.net>
2016-08-15 11:38:07 +0200

    update

--- cmklocal/cmklocal_named-checkconf.sh
+++ cmklocal/cmklocal_named-checkconf.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-# 20160815 (c) Stefan Sticht
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# 20160815 Stefan at Sticht.net
 
 if type named-checkconf > /dev/null
 then
